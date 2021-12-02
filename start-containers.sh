9d61b2489120fa86f5a774508bc6d6335b29b085
Ricardo <rickgboris34@gmail.com>
2021-12-02 10:03:25 +0200

    adding sbt into the docker image

--- start-containers.sh
+++ start-containers.sh
@@ -4,6 +4,6 @@ set -x
 docker-compose up -d --build
 sleep 30
 docker logs pyspark-jupyter
-sleep  60
+sleep  90
 docker-compose exec scylla-manager bash -c 'sctool cluster add --name "Docker Cluster" --host=scylladb-01 --auth-token=token'
 
