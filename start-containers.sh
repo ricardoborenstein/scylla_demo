fb9c3b1dd7b47b81d4743776fdda65efa449d3d8
Ricardo <rickgboris34@gmail.com>
2021-12-02 10:10:04 +0200

    fix scylla-manager add cluster time

--- start-containers.sh
+++ start-containers.sh
@@ -4,6 +4,6 @@ set -x
 docker-compose up -d --build
 sleep 30
 docker logs pyspark-jupyter
-sleep  90
+sleep  120
 docker-compose exec scylla-manager bash -c 'sctool cluster add --name "Docker Cluster" --host=scylladb-01 --auth-token=token'
 
